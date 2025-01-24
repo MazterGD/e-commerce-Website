83f00a830a80468fb87b73c235e65c4f69060716
Geeneth Punchihewa <geenethpunchihewa@gmail.com>
2025-01-25 04:14:04 +0530

    supabase and prisma initial configurations

---
+++ src/app/settings/layout.tsx
@@ -0,0 +1,67 @@
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { prisma } from "../lib/prisma";
+import { MantineProvider } from "@mantine/core";
+import { NavLinks } from "../components/Navbar";
+
+async function getData({
+  email,
+  id,
+  firstName,
+  lastName,
+  profileImage,
+}: {
+  email: string;
+  id: string;
+  firstName: string | undefined | null;
+  lastName: string | undefined | null;
+  profileImage: string | undefined | null;
+}) {
+  const user = await prisma.user.findUnique({
+    where: {
+      id: id,
+    },
+    select: {
+      id: true,
+      stripeCustomerId: true,
+    },
+  });
+
+  if (!user) {
+    const name = `${firstName ?? ""} ${lastName ?? ""}`;
+    await prisma.user.create({
+      data: {
+        id: id,
+        email: email,
+        name: name,
+      },
+    });
+  }
+}
+
+export default async function SettingsLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+  await getData({
+    email: user.email as string,
+    firstName: user.given_name as string,
+    id: user.id as string,
+    lastName: user.family_name as string,
+    profileImage: user.picture,
+  });
+
+  return (
+    <html lang="en">
+      <body>
+        <MantineProvider>
+          {/* <NavLinks /> */}
+          {/* <Navbar /> */}
+          {children}
+        </MantineProvider>
+      </body>
+    </html>
+  );
+}
