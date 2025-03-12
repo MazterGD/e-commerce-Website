11585dcce07aafab45a74fdac5821368183369ca
Geeneth Punchihewa <geenethpunchihewa@gmail.com>
2025-03-13 02:21:23 +0530

    store components

---
+++ src/app/store/types/index.ts
@@ -0,0 +1,13 @@
+export interface Product {
+    id: string
+    name: string
+    description: string
+    price: number
+    size: string
+    popularity: number
+    image: string
+    category: string
+    createdAt: string
+  }
+  
+  
